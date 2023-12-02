0527989c4888db67cbabef16b77842d9b2549e84
Usama Rahman <rahmanusama390@gmail.com>
2023-12-02 22:59:56 +0600

    10 Done

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,16 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route index element={<Homepage />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="product" element={<Product />} />
           <Route path="/login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}></Route>
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>List</p>} />
+            <Route path="cities" element={<p> List of cities </p>} />
+            <Route path="countries" element={<p>Country </p>} />
+            <Route path="form" element={<p> Form </p>} />
+          </Route>
           <Route path="*" element={<Pagenotfound />} />
         </Routes>
       </BrowserRouter>
