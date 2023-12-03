cc15632102edbae4467675cbd144aff2bd3c0f31
Usama Rahman <rahmanusama390@gmail.com>
2023-12-03 07:33:35 +0600

    14

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const URL = "http://localhost:9000/cities";
 
@@ -37,7 +38,7 @@ function App() {
           <Route index element={<Homepage />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="product" element={<Product />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
             <Route
               index
@@ -47,6 +48,7 @@ function App() {
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
+            <Route path="cities/:id" element={<City />} />
             <Route
               path="countries"
               element={<CountryList cities={cities} isLoading={isLoading} />}
