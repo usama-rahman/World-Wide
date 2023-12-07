65fde686f15e0c17b20b1ddce8777c1790f6c821
Usama Rahman <rahmanusama390@gmail.com>
2023-12-07 18:06:25 +0600

    @@

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
+  const { login, isAutenticated } = useAuth;
+  const navigate = useNavigate;
+
+  function handelSubmit(e) {
+    e.preventDefault();
+
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAutenticated) navigate("/app");
+    },
+    [isAutenticated, navigate]
+  );
+
   return (
     <main className={styles.login}>
       <PageNav />
 
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handelSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -33,7 +52,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
