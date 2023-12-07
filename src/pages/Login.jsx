60c5c9a093d80bf12e3868a6775c495daef44ab4
Usama Rahman <rahmanusama390@gmail.com>
2023-12-08 05:26:42 +0600

    👀

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react";
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [password, setPassword] = useState("qwerty👀");
 
   const { login, isAutenticated } = useAuth;
   const navigate = useNavigate;
